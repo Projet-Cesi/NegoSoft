6a324cecf89f510343a4aa4841a221fbb0ec0d5f
Mathys <mathys.ledanois@viacesi.fr>
2024-08-30 11:15:43 +0200

    InitialCommit

---
+++ NegoSoftWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NegoSoftWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
