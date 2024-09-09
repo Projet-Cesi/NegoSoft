1fe6de46a366d2a8933cc260521f2a75700d3f40
MCREVEL <mathis.crevel@viacesi.fr>
2024-09-09 18:06:52 +0200

    Résolution des problèmes de référence et commencement du CRUD pour l'API

---
+++ NegoAPI/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using NegoSoftShared.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using NegoAPI.Services.ProductService;
+using NegoSoftWeb.Data;
+
+
+namespace NegoAPI.Controllers
+{
+
+
+    [ApiController]
+    [Route("[controller]")]
+
+    public class ProductController : ControllerBase
+    {
+
+        private readonly NegoSoftContext dbContext;
+
+        private readonly IProductService productService;
+
+        public ProductController(NegoSoftContext dbContext, IProductService iproductService)
+        {
+            this.dbContext = dbContext;
+            productService = iproductService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProduct()
+        {
+            var products = await productService.GetAllProductAsync();
+
+            if (products == null || !products.Any())
+            {
+                return NotFound("No products found.");
+            }
+
+            return Ok(products);
+        }
+    }
+}
---
+++ NegoAPI/Services/ProductService/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using NegoSoftShared.Models;
+
+namespace NegoAPI.Services.ProductService
+{
+    public interface IProductService 
+    {
+        Task<IEnumerable<NegoSoftShared.Models.Entities.Product>> GetAllProductAsync();
+    }
+}
